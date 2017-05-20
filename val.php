e5460be779fe932b4e465c96ae50d18d6f70765e
fer7sg <fer_sanchez_25_07@hotmail.com>
2017-05-20 01:36:18 -0500

    Add files via upload

--- val.php
+++ val.php
@@ -4,22 +4,29 @@
 		//echo $_POST["cont"]."</br>";
 		$usuario=$_POST["usua"];
 		$contra=$_POST["cont"];
+		$che=$_POST['che'];
 		$con=mysqli_connect("localhost","root","","final");
 		$query="SELECT * FROM usuario WHERE nombre_usuario='$usuario';";
 		$res=mysqli_query($con,$query);
 		$fila=mysqli_fetch_assoc($res);
 		//echo $fila['password'];
-		if($fila==NULL){
-		echo "TU USUARIO NO ESTÁ REGISTRADO";}
-		else{
-			if($contra==$fila['password'])
-			{
-				$pues="tru";
-				echo $pues;
-			}
+		if($fila==NULL)
+		{
+			if($che==0)
+				echo "TU USUARIO NO ESTÁ REGISTRADO";
 			else
-				echo "USUARIO y/o CONTRASEÑA INVALIDOS";
-			}
+				echo "SOLO SE ACEPTAN LETRAS EN USUARIO";
+		}
+		else{
+				if($contra==$fila['password'])
+				{
+					$pues="tru";
+					echo $pues;
+				}
+				else
+					echo "USUARIO y/o CONTRASEÑA INVALIDOS";
+			
+		}
 	}
 	else
 		echo "INGRESE USUARIO Y CONTRASEÑA";
